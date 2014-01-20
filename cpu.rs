9f8abf17190ab313e6dfe0396e43ce652ea3cc92
Breckin Loggins <bloggins@greatfoundry.com>
2014-01-19 22:49:33 -0800

    Start of ALU, more branch instructions and RTS (broken)

--- cpu.rs
+++ cpu.rs
@@ -18,6 +18,7 @@ pub struct CPU {
   PC:             	u16,            // Program Counter Register
 
   priv cc:			uint,			// Current clock cycle (PPU master clock cycles 3x cpu)
+  priv scanline:	int,			// Current scanline
 }
 
 // Opcode encoding format is specified by
@@ -101,71 +102,77 @@ struct OpCode (
 );
 
 impl CPU {
-	pub fn new() -> CPU { CPU { A: 0, X: 0, Y: 0, SP: 0, P: 0, PC: 0, cc: 0 } }
+	pub fn new() -> CPU { CPU { A: 0, X: 0, Y: 0, SP: 0, P: 0, PC: 0, cc: 0, scanline: 0 } }
 
 	pub fn run(&mut self, mem: &mut Mem, startAddr: u16) {
 		self.PC = startAddr;
 		self.SP = 0x10fd;			/* Top of stack (this should really be handled by RESET) */
 		self.fset(Xf);				/* Unused flag is set to 1 at all times */
 		self.fset(If);				/* Interrupts disabled on startup */
+		self.scanline = 241;		/* Attempt to emulate nestest log */
+		loop { self.tick(mem) }
+	}
 
-		let mut scanline : int = 0;		/* TODO: Match nestest.log's starting scanline */
-		loop {
-			let c = mem.read(self.PC);
-			let op = match c {
-				0x08 => OpCode(c, ~"PHP", Impl, 3, |c, m, _ , _ | c.push8(m, c.P)),
-				0x18 => OpCode(c, ~"CLC", Impl, 2, |c, _, _ , _ | c.fclr(Cf)),
-				0x20 => OpCode(c, ~"JSR", Abs,  6, |c, m, a1, a2| { c.push16(m, c.PC); c.PC = a2 as u16 << 8 | a1 as u16 }),
-				0x28 => OpCode(c, ~"PLP", Impl, 4, |c, m, _ , _ | { let src = c.pull(m); c.setreg(Pr, src) }),
-				0x38 => OpCode(c, ~"SEC", Impl, 2, |c, _, _ , _ | c.fset(Cf)),
-				0x48 => OpCode(c, ~"PHA", Impl, 3, |c, m, _ , _ | c.push8(m, c.A)),
-				0x58 => OpCode(c, ~"CLI", Impl, 2, |c, _, _ , _ | c.fclr(If)),
-				0x4C => OpCode(c, ~"JMP", Abs,  3, |c, _, a1, a2| c.PC = a2 as u16 << 8 | a1 as u16),
-				0x68 => OpCode(c, ~"PLA", Impl, 4, |c, m, _ , _ | { c.A = c.pull(m); c.fsetv(Nf, c.A); c.fsetv(Zf, c.A) }),
-				0x78 => OpCode(c, ~"SEI", Impl, 2, |c, _, _ , _ | c.fset(If)),
-				0x84 => OpCode(c, ~"STY", Zpg,  3, |c, m, a1, _ | m.write(a1 as u16, c.Y)),
-				0x85 => OpCode(c, ~"STA", Zpg,  3, |c, m, a1, _ | m.write(a1 as u16, c.A)),
-				0x86 => OpCode(c, ~"STX", Zpg,  3, |c, m, a1, _ | m.write(a1 as u16, c.X)),
-				0x90 => OpCode(c, ~"BCC", Rel,  2, |c, _, a1, _ | if !c.fis_set(Cf) { c.b_rel(a1) }),
-				0xA0 => OpCode(c, ~"LDY", Imm,  2, |c, _, a1, _ | c.setreg(Yr, a1)),
-				0xA2 => OpCode(c, ~"LDX", Imm,  2, |c, _, a1, _ | c.setreg(Xr, a1)),
-				0xA9 => OpCode(c, ~"LDA", Imm,  2, |c, _, a1, _ | c.setreg(Ar, a1)),
-				0xB0 => OpCode(c, ~"BCS", Rel,  2, |c, _, a1, _ | if c.fis_set(Cf) { c.b_rel(a1) }),
-				0xB8 => OpCode(c, ~"CLV", Impl, 2, |c, _, _ , _ | c.fclr(Vf)),
-				0xD0 => OpCode(c, ~"BNE", Rel,  2, |c, _, a1, _ | if !c.fis_set(Zf) { c.b_rel(a1) }),
-				0xD8 => OpCode(c, ~"CLD", Impl, 2, |c, _, _ , _ | c.fclr(Df)),
-				0xEA => OpCode(c, ~"NOP", Impl, 2, |_, _, _ , _ | {} ),
-				0xF0 => OpCode(c, ~"BEQ", Rel,  2, |c, _, a1, _ | if c.fis_set(Zf) { c.b_rel(a1) }),
-				0xF8 => OpCode(c, ~"SED", Impl, 2, |c, _, _ , _ | c.fset(Df)),
-				_    => fail!("unimplemented or illegal instruction 0x{:X}", c)
-			};
+	pub fn tick(&mut self, mem: &mut Mem) {
+		let c = mem.read(self.PC);
+		let op = match c {
+			0x08 => OpCode(c, ~"PHP", Impl, 3, |c, m, _ , _ | c.push8(m, c.P)),
+			0x10 => OpCode(c, ~"BPL", Rel,  2, |c, _, a1, _ | if !c.fis_set(Nf) { c.b_rel(a1) }),
+			0x18 => OpCode(c, ~"CLC", Impl, 2, |c, _, _ , _ | c.fclr(Cf)),
+			0x20 => OpCode(c, ~"JSR", Abs,  6, |c, m, a1, a2| { c.push16(m, c.PC-1); c.PC = a2 as u16 << 8 | a1 as u16 }),
+			0x24 => OpCode(c, ~"BIT", Zpg,  3, |c, m, a1, a2| c.alu_BIT(m, a1, a2)),
+			0x28 => OpCode(c, ~"PLP", Impl, 4, |c, m, _ , _ | { let src = c.pull(m); c.setreg(Pr, src) }),
+			0x38 => OpCode(c, ~"SEC", Impl, 2, |c, _, _ , _ | c.fset(Cf)),
+			0x48 => OpCode(c, ~"PHA", Impl, 3, |c, m, _ , _ | c.push8(m, c.A)),
+			0x50 => OpCode(c, ~"BVC", Rel,  2, |c, _, a1, _ | if !c.fis_set(Vf) { c.b_rel(a1) }),
+			0x58 => OpCode(c, ~"CLI", Impl, 2, |c, _, _ , _ | c.fclr(If)),
+			0x4C => OpCode(c, ~"JMP", Abs,  3, |c, _, a1, a2| c.PC = a2 as u16 << 8 | a1 as u16),
+			0x60 => OpCode(c, ~"RTS", Impl, 6, |c, m, _ , _ | c.PC = ((c.pull(m) as u16) | ((c.pull(m) as u16) << 8))+1),
+			0x68 => OpCode(c, ~"PLA", Impl, 4, |c, m, _ , _ | { c.A = c.pull(m); c.fsetv(Nf, c.A); c.fsetv(Zf, c.A) }),
+			0x70 => OpCode(c, ~"BVS", Rel,  2, |c, _, a1, _ | if c.fis_set(Vf) { c.b_rel(a1) }),
+			0x78 => OpCode(c, ~"SEI", Impl, 2, |c, _, _ , _ | c.fset(If)),
+			0x84 => OpCode(c, ~"STY", Zpg,  3, |c, m, a1, _ | m.write(a1 as u16, c.Y)),
+			0x85 => OpCode(c, ~"STA", Zpg,  3, |c, m, a1, _ | m.write(a1 as u16, c.A)),
+			0x86 => OpCode(c, ~"STX", Zpg,  3, |c, m, a1, _ | m.write(a1 as u16, c.X)),
+			0x90 => OpCode(c, ~"BCC", Rel,  2, |c, _, a1, _ | if !c.fis_set(Cf) { c.b_rel(a1) }),
+			0xA0 => OpCode(c, ~"LDY", Imm,  2, |c, _, a1, _ | c.setreg(Yr, a1)),
+			0xA2 => OpCode(c, ~"LDX", Imm,  2, |c, _, a1, _ | c.setreg(Xr, a1)),
+			0xA9 => OpCode(c, ~"LDA", Imm,  2, |c, _, a1, _ | c.setreg(Ar, a1)),
+			0xB0 => OpCode(c, ~"BCS", Rel,  2, |c, _, a1, _ | if c.fis_set(Cf) { c.b_rel(a1) }),
+			0xB8 => OpCode(c, ~"CLV", Impl, 2, |c, _, _ , _ | c.fclr(Vf)),
+			0xD0 => OpCode(c, ~"BNE", Rel,  2, |c, _, a1, _ | if !c.fis_set(Zf) { c.b_rel(a1) }),
+			0xD8 => OpCode(c, ~"CLD", Impl, 2, |c, _, _ , _ | c.fclr(Df)),
+			0xEA => OpCode(c, ~"NOP", Impl, 2, |_, _, _ , _ | {} ),
+			0xF0 => OpCode(c, ~"BEQ", Rel,  2, |c, _, a1, _ | if c.fis_set(Zf) { c.b_rel(a1) }),
+			0xF8 => OpCode(c, ~"SED", Impl, 2, |c, _, _ , _ | c.fset(Df)),
+			_    => fail!("unimplemented or illegal instruction 0x{:X} at 0x{:X}", c, self.PC)
+		};
 
-			let OpCode(opcode, name, adrmode, cycles, handler) = op;
-			let width = adrmode.width();
-			let (a1, a2) = match width {
-				1 => (0, 0),
-				2 => (mem.read(self.PC+1), 0),
-				3 => (mem.read(self.PC+1), mem.read(self.PC+2)),
-				x => fail!("illegal opcode instruction width {}", x)
-			};
+		let OpCode(opcode, name, adrmode, cycles, handler) = op;
+		let width = adrmode.width();
+		let (a1, a2) = match width {
+			1 => (0, 0),
+			2 => (mem.read(self.PC+1), 0),
+			3 => (mem.read(self.PC+1), mem.read(self.PC+2)),
+			x => fail!("illegal opcode instruction width {}", x)
+		};
 
-			let prev_pc = self.PC;
-			self.PC += width as u16;			// handler may set PC after this
+		let prev_pc = self.PC;
+		self.PC += width as u16;			// handler may set PC after this
 
-			print!("{:04X}  {:02X} {} {}  {} {:28s}", 
-				prev_pc, 
-				opcode, 
-				if width < 2 { ~"  " } else { format!("{:02X}", a1) }, 
-				if width < 3 { ~"  " } else { format!("{:02X}", a2) }, 
-				name, 
-				adrmode.to_str(self, mem, a1, a2));
-			println!("A:{:02X} X:{:02X} Y:{:02X} P:{:02X} SP:{:02X} CYC:{:3u} SL:{:3i}", 
-				self.A, self.X, self.Y, self.P, self.SP & 0x00ff, self.cc, scanline);
+		print!("{:04X}  {:02X} {} {}  {} {:28s}", 
+			prev_pc, 
+			opcode, 
+			if width < 2 { ~"  " } else { format!("{:02X}", a1) }, 
+			if width < 3 { ~"  " } else { format!("{:02X}", a2) }, 
+			name, 
+			adrmode.to_str(self, mem, a1, a2));
+		println!("A:{:02X} X:{:02X} Y:{:02X} P:{:02X} SP:{:02X} CYC:{:3u} SL:{:3i}", 
+			self.A, self.X, self.Y, self.P, self.SP & 0x00ff, self.cc, self.scanline);
 
-			handler(self, mem, a1, a2);
-			self.add_cycles(cycles);
-			scanline = (self.cc / 241) as int - 21;
-		}
+		handler(self, mem, a1, a2);
+		self.add_cycles(cycles);
+		//self.scanline = (self.cc / 241) as int - 21;
 	}
 
 	//
@@ -173,7 +180,13 @@ impl CPU {
 	//
 	fn add_cycles(&mut self, cpu_cycles: u8) {
 		// Convert to master clock (PPU) cycles
+		let prev_cc = self.cc;
 		self.cc = (self.cc + (cpu_cycles as uint) * 3) % 341;
+		if self.cc < prev_cc {
+			// New scanline
+			self.scanline += 1;
+			if self.scanline == 261 { self.scanline = -1 };
+		}
 	}
 
 	//
@@ -200,12 +213,13 @@ impl CPU {
 	//
 
 	fn pull(&mut self, mem: &mut Mem) -> u8 {
+		self.SP += 1;
+		
 		if self.SP == 0x1100 {
 			fail!("stack underflow");
 		}
 
 		let val = mem.read(self.SP);
-		self.SP += 1;
 		return val;
 	}
 
@@ -221,8 +235,8 @@ impl CPU {
 
 	// TODO: Templatize based on width of type
 	fn push16(&mut self, mem: &mut Mem, val: u16) {
-		self.push8(mem, (val & 0x0f) as u8);
-		self.push8(mem, ((val & 0xf0) >> 8) as u8);
+		self.push8(mem, ((val & 0xff00) >> 8) as u8);
+		self.push8(mem, (val & 0x00ff) as u8);
 	}
 
 	//
@@ -236,7 +250,7 @@ impl CPU {
 	fn fsetv(&mut self, f: Flag, val: u8) {
 		match f {
 			Nf => if val & 0x80 == 0 	{ self.fclr(Nf) } else { self.fset(Nf) },
-			Zf => if val == 0 			{ self.fclr(Zf) } else { self.fset(Zf) },
+			Zf => if val == 0 			{ self.fset(Zf) } else { self.fclr(Zf) },
 			_  => fail!("Invalid flag {} sent to fsetv", f as u8)
 		}
 	}
@@ -253,7 +267,7 @@ impl CPU {
 	// Addressing
 	//
 	fn rel_addr(&self, off: u8) -> u16 { 
-		if (off & 0x80 != 0) {
+		if off & 0x80 != 0 {
 			self.PC - ((off & 0x7F) as u16)
 		} else {
 			self.PC + (off as u16) 
@@ -277,5 +291,22 @@ impl CPU {
 				// Crossed page boundary, took two cycles
 				2
 			});
+	}
+
+	//
+	// ALU
+	//
+
+	fn alu_BIT(&mut self, m: &mut Mem, a1: u8, a2: u8)	{
+		let mval = m.read(a1 as u16);
+		let res = mval & self.A;
+
+		self.fsetv(Zf, res);
+		self.fsetv(Nf, mval);
+		if mval & 0x40 != 0 {
+			self.fset(Vf);
+		} else {
+			self.fclr(Vf);
+		}
 	}
 }
